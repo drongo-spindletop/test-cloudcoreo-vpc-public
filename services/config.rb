7306aea97fccd95bf2d8d3d9e9ad9144f66f5216
drongo <aponomarev@spindletop.software>
2016-12-02 17:48:00 +0600

    111

--- services/config.rb
+++ services/config.rb
@@ -9,28 +9,28 @@
 ######################################################################
 ## Create a vpc, igw and attach the igw to the vpc
 ######################################################################
-##coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
-#  action :sustain
-#  cidr "${VPC_OCTETS}/16"
-#  internet_gateway true
-#  region "${REGION}"
-#  tags ${VPC_TAGS}
-#end
+coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
+  action :sustain
+  cidr "${VPC_OCTETS}/16"
+  internet_gateway true
+  region "${REGION}"
+  tags ${VPC_TAGS}
+end
 
 
 ######################################################################
 ## create a routetable for the public subnet, route everything
 ## to the internet gateway
 ######################################################################
-#coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
-#  action :sustain
-#  vpc "${VPC_NAME}${SUFFIX}"
-#  routes [
-#             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
-#        ]
-#  number_of_tables 1
-#  region "${REGION}"
-#end
+coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
+  action :sustain
+  vpc "${VPC_NAME}${SUFFIX}"
+  routes [
+             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
+        ]
+  number_of_tables 1
+  region "${REGION}"
+end
 
 
 ######################################################################
@@ -39,12 +39,12 @@
 ## percent_of_vpc_allocated
 ##   split, but use only this percentage of the entire vpc range
 ######################################################################
-#coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}" do
-#  action :sustain
-#  number_of_zones ${PUBLIC_SUBNET_NUM_ZONES}
-#  percent_of_vpc_allocated 25
-#  route_table "${PUBLIC_ROUTE_NAME}${SUFFIX}"
-#  vpc "${VPC_NAME}${SUFFIX}"
-#  map_public_ip_on_launch true
-#  region "${REGION}"
-#end
+coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}" do
+  action :sustain
+  number_of_zones ${PUBLIC_SUBNET_NUM_ZONES}
+  percent_of_vpc_allocated 25
+  route_table "${PUBLIC_ROUTE_NAME}${SUFFIX}"
+  vpc "${VPC_NAME}${SUFFIX}"
+  map_public_ip_on_launch true
+  region "${REGION}"
+end
